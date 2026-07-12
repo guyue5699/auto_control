b654b239779a4574369ab6a2c3d0eed8409f0d8c
guyue5699 <guyue5699@gmail.com>
2026-07-12 14:24:32 +0800

    1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,16 @@
-rootProject.name = "FacebookGroupSharer"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "自动化群控"
 include(":app")
